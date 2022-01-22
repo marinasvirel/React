0213f262937f00f78fb34d12935a26a73c7483eb
unknown <you@example.com>
2022-01-22 20:21:05 +0300

    add new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import './App.css';
-import Message from './message';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [value, setValue] = useState("");
+
+  const author = {
+    user: "user",
+    bot: "bot",
+  };
+
+  const handleChange = (event) => {
+    const valueFromInput = event.target.value;
+    setValue(valueFromInput);
+  };
+
+  const handleSend = () => {
+    setMessageList([...messageList, { text: value, author: author.user }]);
+    setValue("");
+  };
+
+ useEffect(() => {
+   if(messageList.length > 0 && messageList[messageList.length - 1].author === author.user){
+     setTimeout(() => {
+      setMessageList([...messageList, {
+        text: "Автоматическое сообщение",
+        author: author.bot
+      }]);
+     }, 1500)
+   }
+ }, [messageList])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Мой первый React</h2>
-      </header>
-      <Message text={"Первое задание выполнено"} />
+      <div className="messenger">
+      <div className="dashboard">
+        {messageList.map((message, index) => (
+          <div key={index} className="message">
+            <h6>{message.author}</h6>
+            <p className="messenger-text">{message.text}</p>
+          </div>
+        ))}
+      </div>
+      <div className="control-panel">
+        <input onChange={handleChange} value={value} />
+        <button onClick={handleSend}>Отправить</button>
+      </div>
+      </div>
     </div>
   );
 }
--- src/message.js
+++
@@ -1,9 +0,0 @@
-const Message = (props) => {
-  return(
-    <div>
-      <h3 className="message-title">{props.text}</h3>
-    </div>
-  )
-};
-
-export default Message;
